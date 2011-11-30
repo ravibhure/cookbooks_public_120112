d2419c3603cc2266508c5444c33dd1ebf7f64c4d
Ravi Bhure <ravibhure@gmail.com>
2011-11-30 17:58:28 +0530

    updated rest-client for rs_tools gem in metadata

--- cookbooks/db_postgres/metadata.rb
+++ cookbooks/db_postgres/metadata.rb
@@ -16,6 +16,7 @@ provides "db_postgres_gzipfile_backup(db_name, file_path)"
 provides "db_postgres_gzipfile_restore(db_name, file_path)"
 
 recipe  "db_postgres::default", "Runs the client 'db::install_server' recipes."
+recipe  "db_postgres::gem_rest-client", "Runs the rest-client 'db_postgres::gem_rest-client' recipes."
 
 attribute "db_postgres",
   :display_name => "General Database Options",
