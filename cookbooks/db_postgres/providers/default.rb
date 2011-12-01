a327e4bf405a4c8363ac2e8706e457886d8dfeac
Ravi Bhure <ravibhure@gmail.com>
2011-12-01 20:41:48 +0530

    updated server install

--- cookbooks/db_postgres/providers/default.rb
+++ cookbooks/db_postgres/providers/default.rb
@@ -142,11 +142,6 @@ action :install_client do
   # Install PostgreSQL GPG Key (http://yum.postgresql.org/9.1/redhat/rhel-5-(arch)/pgdg-centos91-9.1-4.noarch.rpm)
     reporpm = ::File.join(::File.dirname(__FILE__), "..", "files", "centos", "pgdg-centos91-9.1-4.noarch.rpm")
     `rpm -ihv #{reporpm}`
-	
-#    pg = execute "install pg_repo rpm" do
-#      command "rpm -ivh --force #{reporpm}"
-#    end
-#    pg.run_action(:run)    
 
     # Packages from postgresql yum repository for PostgreSQL 9.1.1
 #    packages = ["postgresql91-devel", "postgresql91-libs", "postgresql91", "postgresql91-contrib" ]
@@ -208,11 +203,20 @@ action :install_server do
   # PostgreSQL server depends on PostgreSQL client
   action_install_client
 
+  if node[:platform] == "centos"
+  arch = node[:kernel][:machine]
+  arch = "i386" if arch == "x86_64"
+
+
   # == Install PostgreSQL 9.1 and other packages
   #
-  node[:db_postgres][:packages_install].each do |p|
-    package p
-  end unless node[:db_postgres][:packages_install] == ""
+#  node[:db_postgres][:packages_install].each do |p|
+#    package p
+#  end unless node[:db_postgres][:packages_install] == ""
+
+  # Install PostgreSQL server rpm
+    pgdevelrpm = ::File.join(::File.dirname(__FILE__), "..", "files", "centos", "postgresql91-server-9.1.1-1PGDG.rhel5.#{arch}.rpm")
+    `yum -y localinstall #{pgdevelrpm}`
 
   # Uninstall other packages we don't
   node[:db_postgres][:packages_uninstall].each do |p|
