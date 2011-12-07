24a378a121464860f597e8902ea3853149b5e461
Ravi Bhure <ravibhure@gmail.com>
2011-12-07 06:50:07 -0500

    add pg_dump

--- cookbooks/db_postgres/providers/default.rb
+++ cookbooks/db_postgres/providers/default.rb
@@ -299,7 +299,7 @@ action :generate_dump_file do
   bash "Write the postgres DB backup file" do
       user 'postgres'
       code <<-EOH
-      pg_dump #{db_name} | gzip -c > #{dumpfile}
+      pg_dump -h /var/run/postgresql #{db_name} | gzip -c > #{dumpfile}
       EOH
   end
 
