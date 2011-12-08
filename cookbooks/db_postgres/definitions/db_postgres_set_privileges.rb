49a12ef14c9a05ef379f9df46a45b37663a89c73
Ravi Bhure <ravibhure@gmail.com>
2011-12-08 11:51:39 -0500

    updated def admin priv

--- cookbooks/db_postgres/definitions/db_postgres_set_privileges.rb
+++ cookbooks/db_postgres/definitions/db_postgres_set_privileges.rb
@@ -23,7 +23,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-define :db_postgres_set_privileges, :preset => "administrator", :username => nil, :password => nil, :db_name => nil, :admin_role => "admins", :user_role => "user" do 
+define :db_postgres_set_privileges, :preset => "administrator", :username => nil, :password => nil, :db_name => nil, :admin_role => "#{node[:db_postgres][:admin_role]}", :user_role => "#{node[:db_postgres][:user_role]}" do 
 
 
   priv_preset = params[:preset]
