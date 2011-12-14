860b0641ec9299b9029b0d00b89a277116845726
Ravi Bhure <ravibhure@gmail.com>
2011-12-14 16:33:22 +0530

    updated test_db

--- cookbooks/db_postgres/recipes/test_db.rb
+++ cookbooks/db_postgres/recipes/test_db.rb
@@ -1,3 +1,28 @@
+# Cookbook Name:: db_postgres
+# Recipe:: test_db
+#
+# Copyright (c) 2011 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 #
 # Script to download and restore sample database 'booktown' db in to postgres
 # Ravi Bhure
@@ -8,6 +33,8 @@ dumpfile = /tmp/baooktown.sql
 
 bash "download and restore sample database" download  do
     code <<-EOF
+      # Downloading #{dumpfile}
+      log "Downloading #{dumpfile}......"
       wget http://www.commandprompt.com/ppbook/booktown.sql  -O #{dumpfile}
       set -e
       if [ ! -f #{dumpfile} ]
@@ -15,6 +42,6 @@ bash "download and restore sample database" download  do
         echo "ERROR: PostgreSQL sample database file not found! File: '#{dumpfile}'"
         exit 1
       fi
-      psql -h /var/run/postgres -U postgres < #{dumpfile}
+      psql -h /var/run/postgresql -U postgres < #{dumpfile}
     EOF
 end
