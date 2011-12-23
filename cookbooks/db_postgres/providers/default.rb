5573819038b5650565f53e60fc0cc238a0deb3d9
Ravi Bhure <ravibhure@gmail.com>
2011-12-23 22:38:44 +0530

    updated enable-promote

--- cookbooks/db_postgres/providers/default.rb
+++ cookbooks/db_postgres/providers/default.rb
@@ -397,7 +397,7 @@ action :promote do
 
   ### INITIAL CHECKS 
   # Perform an initial connection forcing to accept the keys...to avoid interaction.
-    db.accept_ssh_key(newmaster)
+    #db.accept_ssh_key(newmaster)
 
   # Ensure that that the newmaster DB is up
     action_start
